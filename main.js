9921de9c41f4ab79c074218d3fec0f6b0dc55600
sylvoee <sylvoee@gmail.com>
2022-09-02 09:13:12 +0100

    Commiting all files

---
+++ main.js
@@ -0,0 +1,35 @@
+
+
+// selecting element
+let btnInterest = document.querySelector(".simple-interest");
+let btnDisplayInterest = document.querySelector(".display-interest");
+
+let inputAmount = document.querySelector(".amount");
+let inputRate = document.querySelector(".rate");
+let inputTime = document.querySelector(".time");
+
+// function cal interest
+function interest(p, r, t){
+    let i = p * r * t /100
+    // console.log("Your interst is " + i);
+    btnDisplayInterest.innerHTML = "Your interst after  " + t + " years of investing  $" + p +
+     " at " + r +"% rate is " + '<b>$' + i + '</b>'
+
+}
+
+
+
+
+// adding an event listener
+btnInterest.addEventListener('click', ()=>{
+
+
+    let floatAmount = parseFloat(inputAmount.value);
+    let floatRate = parseFloat(inputRate.value);
+    let floatTime = parseFloat(inputTime.value);
+   
+    // calling the dunction interest
+   interest(floatAmount, floatRate ,floatTime)
+
+});
+
